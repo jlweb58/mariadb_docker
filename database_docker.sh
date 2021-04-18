976ba2f9f03c865fb5551ef62b52cba5fc10208d
John Webber <jlweb58@gmail.com>
2021-04-18 09:30:07 +0200

    Copy of script added

---
+++ database_docker.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+cd ~/mariadb_docker
+
+# Update the repository
+git pull
+
+# Copy the latest db version
+DATABASE=database_latest.sql
+gunzip ../$DATABASE.gz
+cp ../$DATABASE .
+
+# If this fails the rest won't work
+docker login
+docker build . -t jlweb58/jlweb58-repo:database_dump-latest
+docker push jlweb58/jlweb58-repo:database_dump-latest
+echo "Building and pushing docker image succeeded"
+
+
+
+
